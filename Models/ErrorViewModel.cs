0625137952d0408ab223684dadd4190e250de137
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2021-11-21 14:04:54 -0700

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace J_C_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
